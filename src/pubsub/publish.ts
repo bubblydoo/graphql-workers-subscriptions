c6d79379f81a7890b7df51df418ebb2e20a98f0f
izidormaklary <maklaryizidor@gmail.com>
2023-01-14 12:49:31 +0100

    switch to payload coming from request and add ws-sub id

--- src/pubsub/publish.ts
+++ src/pubsub/publish.ts
@@ -9,30 +9,30 @@ export const publish =
     const subscriptions: Subscription[] = await getFilteredSubs(env, event);
 
     const iters = subscriptions.map(async (sub) => {
-      const payload = await execute({
-        schema: schema,
-        document: parse(sub.subscription.query),
-        // contextValue: await buildContext({
-        //   server,
-        //   connectionInitPayload: sub.connectionInitPayload,
-        //   connectionId: sub.connectionId,
-        // }),
-        variableValues: sub.subscription.variables,
-        operationName: sub.subscription.operationName,
-      });
-      debugger;
+    //   const payload = await execute({
+    //     schema: schema,
+    //     document: parse(sub.subscription.query),
+    //     rootValue: event,
+    //     // contextValue: await buildContext({
+    //     //   server,
+    //     //   connectionInitPayload: sub.connectionInitPayload,
+    //     //   connectionId: sub.connectionId,
+    //     // }),
+    //     variableValues: sub.subscription.variables,
+    //     operationName: sub.subscription.operationName,
+    //   });
 
       const message: NextMessage = {
-        id: sub.connectionId,
+        id: sub.id,
         type: MessageType.Next,
-        payload,
+        payload: { data: event.payload },
       };
 
       const DOId = env.WS_CONNECTION.idFromString(sub.connectionId);
       const DO = env.WS_CONNECTION.get(DOId);
       DO.fetch("https://websocket.io/publish", {
         method: "POST",
-        body: JSON.stringify({ data: message }),
+        body: JSON.stringify(message),
       });
 
       // ((server)({
