604c4fed335e8566b586c219f093c73bd6936b39
Hans Otto Wirtz <hansottowirtz@gmail.com>
2023-01-17 22:43:10 +0800

    change to yarn, add build app script

--- src/app/app.ts
+++ src/app/app.ts
@@ -18,6 +18,7 @@ export const schema = makeExecutableSchema<DefaultPublishableContext<ENV>>({
     type Greeting {
       greeting: String
     }
+    type Query {}
     type Subscription {
       greetings(greeting: String): Greeting
     }
